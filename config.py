73a5e5914565f45d71e17be40a033e4f07dfb6a1
Eduardo Cuducos <user@chrubuntu.(none)>
2014-01-11 01:43:11 +0000

    Changed DB from SQLite to Postgres

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/whiskyton' 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 MAIN_TITLE = 'Whiskyton'
 HEADLINE = 'Find whiskies that you like!'
