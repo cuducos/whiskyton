f2d575b1e9b6e0e795c1a9a2a26430bcdf4bd9fa
Eduardo Cuducos <user@chrubuntu.(none)>
2014-01-11 21:17:31 +0000

    Add $DATABASE_URL to config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 MAIN_TITLE = 'Whiskyton'
 HEADLINE = 'Find whiskies that you like!'
