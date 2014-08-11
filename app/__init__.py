f28dd9faff62dce250014c8a1c01c5efbd68f74f
Eduardo Cuducos <cuducos@gmail.com>
2014-08-11 15:59:48 +0200

    PEP-8

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.assets import Environment, Bundle
+
 # init app and db
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 from app import views, models
+
 # SCSS
 assets = Environment(app)
 assets.url = app.static_url_path
