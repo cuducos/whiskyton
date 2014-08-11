4f81b5edd5c67240da2b6cf68494afe86fa86efd
Eduardo Cuducos <cuducos@gmail.com>
2014-08-11 17:22:11 +0200

    Fix bloodhound loading

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 
 	// typeahed + bloodhound
+	var whiskies = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		limit: 10,
+		prefetch: {
+			url: '/whiskyton.json'		
+		}
+	});
 	whiskies.initialize();
 	$('#s').typeahead(null, {
 		name: 'whiskies',
@@ -10,11 +18,4 @@ $(document).ready(function(){
 	
 });
 
-var whiskies = new Bloodhound({
-	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-	queryTokenizer: Bloodhound.tokenizers.whitespace,
-	limit: 10,
-	prefetch: {
-		url: '/whiskyton.json'		
-	}
-});
+	
