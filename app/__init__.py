34816e3349586aff9525c46c8e9cdf9b536c1fda
Eduardo Cuducos <cuducos@gmail.com>
2014-08-11 15:29:03 +0200

    Implement SCSS

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.assets import Environment, Bundle
 
+# init app and db
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
 from app import views, models
+
+# SCSS
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('scss/style.scss', 'scss/typeahead.fix.scss', filters='pyscss', output='css/style.css')
+assets.register('scss_style', scss)
