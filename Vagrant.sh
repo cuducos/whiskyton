b00a0bc400d2de9596e7a9d0644ae3419d1f6a71
Eduardo Cuducos <cuducos@gmail.com>
2014-11-30 20:18:16 +0000

    Add iPython and Bower

--- Vagrant.sh
+++ Vagrant.sh
@@ -2,7 +2,7 @@
 
 # Python
 apt-get update
-sudo apt-get install -y python-setuptools
+sudo apt-get install -y python-setuptools ipython
 easy_install pip
 
 # Node.js and CoffeeScript
@@ -10,6 +10,7 @@ apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 apt-get install -y nodejs
 npm install -g coffee-script
+npm install -g bower
 
 # install project dependencies
 pip install -r /vagrant/requirements.txt
